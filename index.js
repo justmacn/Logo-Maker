02afe5abaa033a350739f8a903b5e02eca1c3be7
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-25 23:41:56 -0400

    added inquirer module and function

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 // require modules
 const inquirer = require('inquirer');
 
+// an array of prompt questions
+const questions = [
+    'Enter 3 characters for your logo text:',
+    'Enter a text color:',
+    'Select your logo shape:',
+    'Enter a shape color:'
+]
+
 // function to run on initialize
 function init() {
-
-}
+    inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "text",
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "textColor",
+        },
+        {
+            type: "list",
+            message: questions[2],
+            name: "shape",
+            choices: [
+                "Circle",
+                "Triangle",
+                "Square",
+            ]
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "shapeColor",
+        }
+    ])
+        .then((answers) => console.log(answers));
+};
 
 // call initialize function
 init();
